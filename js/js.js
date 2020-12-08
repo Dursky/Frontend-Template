ff121a4c856da2d0b242ab9a469a7a53557c766c
WounderDragon <46166716+WounderDragon@users.noreply.github.com>
2020-12-08 18:47:20 +0100

    make more features

--- js/js.js
+++ js/js.js
@@ -1,5 +1,23 @@
-function search(){
-    if($("#text_search" ).length < 1){
-        $( "<div id='text_search'></div>" ).insertAfter( "#search" );
+$(document).ready(function(){    
+var click = 1;
+$("#search").click(function () {
+    if(click == 2){
+        //If secod click remove a search bar and reset a click variable
+        $("#text_search").remove();
+        click = 0;
     }
-}
+    if($("#text_search" ).length < 1 && click == 1 ){
+        //Add div when not be in DOM and first click on button
+        $( "<div id='text_search'></div>" ).insertAfter( "#search" );
+        $( "#text_search" ).append( "<div id='search_btn'>DALEJ</div>"  );
+        
+    }
+    click++;
+
+});
+
+
+$('#hamburger').click(function() {
+
+});
+});
