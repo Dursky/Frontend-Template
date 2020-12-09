415627b7fd254a9d334dc898f3b35c41f8664072
WounderDragon <46166716+WounderDragon@users.noreply.github.com>
2020-12-09 21:32:24 +0100

    add some little update
    I added a button on menu,complete a search bar with animation, to be done i need to make a menu category with toggle button.

--- js/js.js
+++ js/js.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){    
 var click = 1;
+var show = 1;
 $("#search").click(function () {
     if(click == 2){
         //If secod click remove a search bar and reset a click variable
@@ -9,7 +10,8 @@ $("#search").click(function () {
     if($("#text_search" ).length < 1 && click == 1 ){
         //Add div when not be in DOM and first click on button
         $( "<div id='text_search'></div>" ).insertAfter( "#search" );
-        $( "#text_search" ).append( "<div id='search_btn'>DALEJ</div>"  );
+        $( "#text_search" ).append( "<div id='search_btn'><p style='margin-top:5px;'>DALEJ</p></div>"  );
+        $( "#text_search" ).append( "<input type text id='search_input'/>"  );
         
     }
     click++;
@@ -18,6 +20,19 @@ $("#search").click(function () {
 
 
 $('#hamburger').click(function() {
-
+    if(show == 1){
+$("#text_title").hide();
+    }
+if(show ==2){
+    $("#text_title").show();
+    show = 0;
+}
+show++;
 });
+
+
+$(".plus").click(function () {
+console.log(this.id);
+});
+
 });
